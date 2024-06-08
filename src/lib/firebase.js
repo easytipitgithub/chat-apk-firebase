c8ca49abcafeea20b8aba94f3b7d409952c98d21
Tanya Dixit <120579321+easytipitgithub@users.noreply.github.com>
2024-06-08 14:51:03 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,14 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCKDIgRNtK-CsG38jrBjF7b-2B4gsmOKSI",
-    authDomain: "chat-app-eff7e.firebaseapp.com",
-    projectId: "chat-app-eff7e",
-    storageBucket: "chat-app-eff7e.appspot.com",
-    messagingSenderId: "768036085164",
-    appId: "1:768036085164:web:5a4c906b083977aecb2e09"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyCKDIgRNtK-CsG38jrBjF7b-2B4gsmOKSI",
+  authDomain: "chat-app-eff7e.firebaseapp.com",
+  projectId: "chat-app-eff7e",
+  storageBucket: "chat-app-eff7e.appspot.com",
+  messagingSenderId: "768036085164",
+  appId: "1:768036085164:web:5a4c906b083977aecb2e09"
+};
+
 
 const app = initializeApp(firebaseConfig);
 
